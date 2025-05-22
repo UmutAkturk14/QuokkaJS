b6a1e93bcf33ea22a1d7de8b41e94a81ee51b863
Umut Akturk <umut.akturk14@gmail.com>
2025-05-22 13:28:38 +0300

    Configuration done.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/main.ts',
-  output: {
-    file: 'dist/main.js',
-    format: 'iife',
-    name: 'Q',
-    sourcemap: true
+export default [
+  {
+    input: 'src/main.ts',
+    output: [
+      { file: 'dist/quokka.cjs.js', format: 'cjs', exports: 'named' },
+      { file: 'dist/quokka.es.js', format: 'es', exports: 'named' },
+      { file: 'dist/quokka.iife.js', format: 'iife', name: 'Q' }
+    ],
+    plugins: [typescript()]
   },
-  plugins: [typescript()]
-};
+  {
+    input: 'src/main.ts',
+    output: { file: 'dist/main.d.ts', format: 'es' },
+    plugins: [dts()]
+  }
+];
